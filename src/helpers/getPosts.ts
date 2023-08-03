24627c75182a8c7fb7f7f4b3acb71645fe0a5986
Javier Andres <79766563+dresandev@users.noreply.github.com>
2023-08-03 02:12:37 -0500

    Test

--- src/helpers/getPosts.ts
+++ src/helpers/getPosts.ts
@@ -5,14 +5,14 @@ import fs from 'fs/promises'
 import type { Post } from '@types'
 
 const getPosts = cache(async () => {
-  const relativeFilePath = './src/posts/'
-  const posts = await fs.readdir(relativeFilePath)
+  const postsDirectory = path.resolve(process.cwd(), './src/posts/') // Obtener la ruta completa a la carpeta "posts"
+  const posts = await fs.readdir(postsDirectory)
 
   return Promise.all(
     posts
       .filter(file => path.extname(file) === '.mdx')
       .map(async file => {
-        const filePath = `${relativeFilePath}${file}`
+        const filePath = path.join(postsDirectory, file) // Utilizar path.join para construir la ruta completa del archivo
         const postContent = await fs.readFile(filePath, 'utf8')
         const { data, content } = matter(postContent)
 
